5e376834a7f1d48ef5655ba038dbc0831b1aee29
lonelyenvoy <lonelyenvoy@gmail.com>
2019-04-25 15:32:56 +0800

    update examples

--- memoization/examples.py
+++ memoization/examples.py
@@ -1,5 +1,8 @@
-from memoization import cached
+from memoization import cached, CachingAlgorithmFlag
 import timeit
+
+depth = 500
+test_times = 100
 
 
 def factorial(n):
@@ -9,7 +12,7 @@ def factorial(n):
     return n * factorial(n - 1)
 
 
-@cached()
+@cached(max_size=64, algorithm=CachingAlgorithmFlag.FIFO, thread_safe=False)
 def quick_factorial(n):
     assert n >= 0
     if n == 0 or n == 1:
@@ -18,18 +21,17 @@ def quick_factorial(n):
 
 
 def test1():
-    for i in range(1, 100):
+    for i in range(1, 500):
         factorial(i)
 
 
 def test2():
-    for i in range(1, 100):
+    for i in range(1, 500):
         quick_factorial(i)
 
 
-test_times = 1000
 time1 = timeit.timeit(test1, number=test_times) / test_times
 time2 = timeit.timeit(test2, number=test_times) / test_times
 
-print('factorial(100) without memoization took ' + str(time1 * 1000) + ' ms')
-print('factorial(100) with    memoization took ' + str(time2 * 1000) + ' ms')
+print('factorial(' + str(depth) + ') without memoization took ' + str(time1 * 1000) + ' ms')
+print('factorial(' + str(depth) + ') with    memoization took ' + str(time2 * 1000) + ' ms')

292e0fb937ce144dbb3fb9c3482cbcd5d87274a6
lonelyenvoy <lonelyenvoy@gmail.com>
2020-01-12 01:15:03 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='memoization',
-    version='0.2.1',
+    version='0.2.2',
     description='A powerful caching library for Python, with TTL support and multiple algorithm options. '
                 '(https://github.com/lonelyenvoy/python-memoization)',
     long_description=long_description,
